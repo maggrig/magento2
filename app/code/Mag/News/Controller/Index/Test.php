538f47d6801d5a8803db73b4838de7ffef5a1f96
maggrig <grigoriy1@rambler.ru>
2019-03-18 10:56:04 +0200

    18.03.2019 10:55

--- app/code/Mag/News/Controller/Index/Test.php
+++ app/code/Mag/News/Controller/Index/Test.php
@@ -7,7 +7,7 @@ class Test extends \Magento\Framework\App\Action\Action
 
     public function execute()
     {
-        $textDisplay = new \Magento\Framework\DataObject(array('text' => 'Mageplaza'));
+        $textDisplay = new \Magento\Framework\DataObject(array('text' => 'hi all'));
         $this->_eventManager->dispatch('catalog_category_prepare_save', ['mp_text' => $textDisplay]);
         echo $textDisplay->getText();
         exit;
